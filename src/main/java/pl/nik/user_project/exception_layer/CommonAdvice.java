c39a6def091744d03e785dcf1b73ac363585d3e7
Nikalaj87 <traderok@ukr.net>
2023-09-17 21:15:48 +0200

    validationException Added

--- src/main/java/pl/nik/user_project/exception_layer/CommonAdvice.java
+++ src/main/java/pl/nik/user_project/exception_layer/CommonAdvice.java
@@ -2,16 +2,35 @@ package pl.nik.user_project.exception_layer;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import pl.nik.user_project.exception_layer.exceptions.BadRequestBodyJson;
 import pl.nik.user_project.exception_layer.exceptions.NoNikalajException;
 
-@ControllerAdvice
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class CommonAdvice {
 
     @ExceptionHandler(NoNikalajException.class)
     public ResponseEntity<Response> handleException (NoNikalajException e) {
         Response response = new Response(e.getMessage());
         return new ResponseEntity<>(response, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<BadRequestBodyJson> handlerValidationException(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError)error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(new BadRequestBodyJson(errors),
+                HttpStatus.BAD_REQUEST);
     }
 }
